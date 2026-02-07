46383a9a6c685bc747311b343a4885ef73b9feb2
Nicolas Silva <nic.silvaa@duocuc.cl>
2026-02-07 13:54:31 -0300

    Add ui.shadcn.com component library

---
+++ src/components/table-travels.tsx
@@ -0,0 +1,105 @@
+import { MoreHorizontalIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '@/components/ui/table'
+
+export function TableTravels() {
+	return (
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<TableHead>Product</TableHead>
+					<TableHead>Price</TableHead>
+					<TableHead className="text-right">Actions</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				<TableRow>
+					<TableCell className="font-medium">Wireless Mouse</TableCell>
+					<TableCell>$29.99</TableCell>
+					<TableCell className="text-right">
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button
+									variant="ghost"
+									size="icon"
+									className="size-8"
+								>
+									<MoreHorizontalIcon />
+									<span className="sr-only">Open menu</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuItem>Edit</DropdownMenuItem>
+								<DropdownMenuItem>Duplicate</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell className="font-medium">Mechanical Keyboard</TableCell>
+					<TableCell>$129.99</TableCell>
+					<TableCell className="text-right">
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button
+									variant="ghost"
+									size="icon"
+									className="size-8"
+								>
+									<MoreHorizontalIcon />
+									<span className="sr-only">Open menu</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuItem>Edit</DropdownMenuItem>
+								<DropdownMenuItem>Duplicate</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell className="font-medium">USB-C Hub</TableCell>
+					<TableCell>$49.99</TableCell>
+					<TableCell className="text-right">
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button
+									variant="ghost"
+									size="icon"
+									className="size-8"
+								>
+									<MoreHorizontalIcon />
+									<span className="sr-only">Open menu</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuItem>Edit</DropdownMenuItem>
+								<DropdownMenuItem>Duplicate</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</TableCell>
+				</TableRow>
+			</TableBody>
+		</Table>
+	)
+}
